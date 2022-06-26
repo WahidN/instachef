44da8603f5fcb0e2d42f100ebb4cf7fa788f3aa1
Wahid <wahid.nawzadi@gmail.com>
2022-06-26 16:03:46 +0200

    add font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
@@ -8,6 +8,11 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#137974" />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;700&display=swap"
+            rel="stylesheet"
+          ></link>
         </Head>
         <body>
           <Main />
