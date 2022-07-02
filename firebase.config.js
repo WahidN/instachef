bf5a965dba6b66521c37b81eb63b5ab24b0909b6
Wahid <wahid.nawzadi@gmail.com>
2022-07-02 12:41:31 +0200

    add firebase

---
+++ firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBIeAK658CH3bjXjmUnpIskKJWYWGicOvI',
+  authDomain: 'instachef-4e37a.firebaseapp.com',
+  projectId: 'instachef-4e37a',
+  storageBucket: 'instachef-4e37a.appspot.com',
+  messagingSenderId: '325887410986',
+  appId: '1:325887410986:web:519d428c8e3c135e6fd453',
+  measurementId: 'G-FH8DL93YLF',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
