64744fb5fc514e7db90c159f8828e2205ac5d81d
Wahid Nawzadi <dev@wahidnawzadi.com>
2022-09-21 20:38:03 +0200

    change header color

--- components/headers/DefaultHeader.tsx
+++ components/headers/DefaultHeader.tsx
@@ -1,6 +1,6 @@
 import styles from 'components/headers/Header.module.css';
 import { Logo } from 'components/Logo';
-import React, { ReactNode, useMemo } from 'react';
+import React, { ReactNode } from 'react';
 
 interface Properties {
   showLogo?: boolean;
@@ -9,16 +9,12 @@ interface Properties {
   buttonRight?: ReactNode;
 }
 
-export const DefaultHeader = React.memo(({ title, showLogo, buttonLeft, buttonRight }: Properties) => {
-  const classes = useMemo(() => (!showLogo ? styles.headerWhite : undefined), [showLogo]);
-
-  return (
-    <div className={`${styles.headerInner} ${classes}`}>
-      <div className={styles.buttonLeft}>{buttonLeft}</div>
-      {showLogo ? <Logo smallLogo /> : <span className={styles.title}>{title}</span>}
-      <div className={styles.buttonRight}>{buttonRight}</div>
-    </div>
-  );
-});
+export const DefaultHeader = React.memo(({ title, showLogo, buttonLeft, buttonRight }: Properties) => (
+  <div className={styles.headerInner}>
+    <div className={styles.buttonLeft}>{buttonLeft}</div>
+    {showLogo ? <Logo smallLogo /> : <span className={styles.title}>{title}</span>}
+    <div className={styles.buttonRight}>{buttonRight}</div>
+  </div>
+));
 
 DefaultHeader.displayName = 'DefaultHeader';
